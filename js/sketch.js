dd644007ee78e969c0d6fd79f1383c32c1bcd5a5
bryanlohjy <bryanlohjy@hotmail.com>
2016-07-03 21:42:32 +1200

    Padding on Tiles

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,24 @@
 function setup(){
-	var canvas = createCanvas(windowWidth,400);
+	var canvas = createCanvas(window.innerWidth, window.innerHeight);
 	canvas.parent("canvasContainer");
-
-
+	// colorMode(HSB);
+	noStroke();	
 }
 
 function draw(){
-	background(200,0,0);
-	fill(255,0,0);
+	push();
+	fill(80,20,200,30);
+	rect(0,0,width,height);
+	pop();
+
+	push();
+	fill(0,20,10);
+	ellipse(mouseX,mouseY,20,20);
+	pop();
 }
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+}
+
+
